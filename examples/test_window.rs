270e8022ca771a0ff70a3b215d62a233891e6d5e
dfghgfd-xyz <uun.metasauce@gmail.com>
2020-11-07 12:41:29 +0300

    Updated to latest egui and miniquad

--- examples/test_window.rs
+++ examples/test_window.rs
@@ -65,7 +65,7 @@ impl EventHandler for Stage {
         // TODO: handle this output so that hyperlinks, etc. work
         let (_, paint_jobs) = self.egui_ctx.end_frame();
 
-        self.painter.paint(ctx, paint_jobs, self.egui_ctx.texture());
+        self.painter.paint(ctx, paint_jobs, &self.egui_ctx.texture());
     }
 }
 
